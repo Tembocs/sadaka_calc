5fd9f22704518b1d985af9f1d541fdcfd63fb0b7
Tembo Nyati <tembocs101@gmail.com>
2019-01-03 00:51:31 +0300

    Redesigned home page to make cards.

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,40 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(SadakaCalc());
 
-class MyApp extends StatelessWidget {
+class SadakaCalc extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Kokotoa Zaka',
+      title: 'Kokotoa Mgawanyo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.blue,
-        textTheme: TextTheme(
-          display1: TextStyle(
-            color: Colors.black,
-            fontSize: 20,
-          ),
-        ),
+//        textTheme: TextTheme(
+//          display1: TextStyle(
+//            color: Colors.black,
+//            fontSize: 20,
+//          ),
+//        ),
       ),
 
-      home: MyHomePage(title: 'Kokotoa Mgawanyo wa Sadaka'),
+      home: MainHomePage(title: 'Kokotoa Mgawanyo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class MainHomePage extends StatefulWidget {
+  MainHomePage({Key key, this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MainHomePageState createState() => _MainHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MainHomePageState extends State<MainHomePage> {
+  double _fontsize = 20;
   double _userSadaka = 0;
   double _sadakaECT = 0;
   double _sadakaKanisani = 0;
@@ -64,12 +48,10 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void _calculateSadakaPercentage() {
-    // This should be called after the calculate button is pressed
     setState(() {
       _userSadaka = double.parse(inputController.text);
-      // what if _userSadaka is 0? Pop up alert dialog
       _sadakaECT = _userSadaka * 0.58;
-      _sadakaKanisani = _userSadaka * 0.43;
+      _sadakaKanisani = _userSadaka * 0.42;
     });
   }
 
@@ -80,6 +62,24 @@ class _MyHomePageState extends State<MyHomePage> {
           _sadakaKanisani = 0;
     });
   }
+
+  // Card decorations
+  BoxDecoration _decorateCard() {
+    return BoxDecoration(
+      color: Colors.white,
+      border: Border.all(color: Colors.blueAccent),
+      borderRadius: BorderRadius.circular(10.0),
+
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey,
+          offset: Offset(3, 3),
+          blurRadius: 3
+        )
+      ]
+    );
+  }
+
   // So that we have instant results as user types
   @override
   void initState() {
@@ -97,90 +97,149 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
+      // To avoid 'Bottom overflowed by xyz pixels' error message
+      // Consider using one of scrolling widgets, e.g. ListView,
+      // NestedScrollView, GridView, SingleChildScrollView, Scrollable.
+      resizeToAvoidBottomPadding: false,
+
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Padding(
-          padding: EdgeInsets.all(40.0),
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Kiasi cha sadaka:',
-              style: Theme.of(context).textTheme.display1,
-            ),
+        child: Container(
+          margin: EdgeInsets.all(10.0),
 
-            TextField(
-              style: Theme.of(context).textTheme.display1,
-              decoration: InputDecoration(
-                //labelText: 'Kiasi cha sadaka:',
-                prefixText: 'Tsh. ',
+          child: Column(
+            children: <Widget>[
+              Expanded(
 
-                // For clearing input field
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.clear),
-      
-                  onPressed: () {
-                    inputController.clear();
-                    _clearSadakaPercentage();
-                  },
-                )
+                // First card with TextFormField
+                child: Container(
+                  margin: const EdgeInsets.all(10.0),
+                  //color: Colors.lightBlueAccent,
+                  constraints: BoxConstraints.expand(),
+                  alignment: Alignment(0, 0),
+                  decoration: _decorateCard(),
+
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        margin: EdgeInsets.all(10),
+                        child: Text(
+                          "Sadaka",
+                          style: TextStyle(
+                            fontSize: _fontsize,
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        margin: EdgeInsets.only(left: 40, right: 40),
+                        child: TextField(
+                          autofocus: true,
+                          keyboardType: TextInputType.number,
+                          controller: inputController,
+
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: _fontsize,
+                          ),
+                          
+                          decoration: InputDecoration(
+                            suffixIcon: IconButton(
+                              icon: Icon(Icons.clear),
+
+                              onPressed: () {
+                                inputController.clear();
+                                _clearSadakaPercentage();
+                              }
+                            )
+                          )
+                        )
+                      )
+                    ],
+                  )
+                  //child: Text("First item, Expanded"),
+                ),
+
               ),
-              autofocus: true,
-              keyboardType: TextInputType.number,
-              controller: inputController,
-            ),
 
-            // Spacer(flex: 1),
+              Expanded(
+                // Second card with TextFormField
+                child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    //color: Colors.lightBlueAccent,
+                    constraints: BoxConstraints.expand(),
+                    alignment: Alignment(0, 0),
 
-            Text(
-              'Sadaka ya Pomoja ECT (58%):',
-              style: Theme.of(context).textTheme.display1,
-            ),
+                    decoration: _decorateCard(),
 
-            Text(
-              '$_sadakaECT',
-              style: Theme.of(context).textTheme.display1,
-            ),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Text(
+                            "ECT (58%)",
+                            style: TextStyle(
+                              fontSize: _fontsize
+                            ),
+                          ),
+                        ),
 
-            // Spacer(flex: 1),
-            Text(
-              'Sadaka ya Pomoja Kanisani (58%):',
-              style: Theme.of(context).textTheme.display1,
-            ),
+                        Container(
+                          child: Text(
+                            '${_sadakaECT.toStringAsFixed(1)}',
+                            style: TextStyle(
+                              fontSize: _fontsize
+                            ),
+                          )
+                        )
+                      ],
+                    )
+                  //child: Text("First item, Expanded"),
+                ),
+              ),
 
-            Text(
-              '$_sadakaKanisani',
-              style: Theme.of(context).textTheme.display1,
-            ),
+              Expanded(
+                // Third card with TextFormField
+                child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    //color: Colors.lightBlueAccent,
+                    constraints: BoxConstraints.expand(),
+                    alignment: Alignment(0, 0),
 
-            // With instant value changes this button is redundant.
-            // RaisedButton(
-            //   child: Text('Kokotoa'),
-            //   onPressed: _calculateSadakaPercentage,
-            // ),
-          ],
+                    decoration: _decorateCard(),
+
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.all(10),
+                          child: Text(
+                            "Kanisani (42)",
+                            style: TextStyle(
+                              fontSize: _fontsize
+                            ),
+                          ),
+                        ),
+
+                        Container(
+                            child: Text(
+                              '${_sadakaKanisani.toStringAsFixed(1)}',
+                              style: TextStyle(
+                                fontSize: _fontsize
+                              ),
+                            )
+                        )
+                      ],
+                    )
+                  //child: Text("First item, Expanded"),
+                ),
+              )
+            ],
+          )
         ),
-      ),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:zaka/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(SadakaCalc());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
